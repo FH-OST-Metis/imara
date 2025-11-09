3e2c0847254c84fe0a535b44ea47674450d44c44
Lukas Koller <koller@puzzle.ch>
2025-11-09 16:09:33 +0100

    Update

--- src/app/extract.py
+++ src/app/extract.py
@@ -8,6 +8,6 @@ for file in src.rglob("*"):
     if file.is_file():
         target = dst / file.relative_to(src)
         target.parent.mkdir(parents=True, exist_ok=True)
-        shutil.move(str(file), str(target))
+        shutil.copy(str(file), str(target))
 
 print("✅ Done: all files moved recursively.")
